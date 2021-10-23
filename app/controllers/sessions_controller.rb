d92a7a88ba10a803085b8063a0ff0fca42aaefaa
IvanWiesner <ivanwiesner@gmail.com>
2021-10-22 18:58:21 -0600

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
     def create 
         user = User 
             .find_by(email: params["user"]["email"])
@@ -15,4 +16,16 @@ class SessionsController < ApplicationController
                 render json: { status: 401 }
             end
         end
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
 end
